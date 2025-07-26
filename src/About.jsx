dfe79864a8150eecd4ce211d43ccf5056aefffcb
Onuche atabo <onuchevictor.dev.2023@gmail.com>
2025-07-26 09:57:59 +0100

    about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,19 +1,39 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const About = () => {
   return (
     <div>
       <>
-        About
         <section id="about" class="bio-snippet">
-          <h2>About Me</h2>
-          <p>
-            👋 Hi, I’m <strong>Onuche Atabo</strong> — an{" "}
-            <strong>IT Support Specialist</strong> and{" "}
-            <strong>Frontend Developer</strong>
-            passionate about solving real-world problems with technology.
+          <h2 className="flex font-rubik text-justify text- text-5xl pt-6 font-bold justify-center text-amber-600 align-middle">
+            About Me
+          </h2>
+          <p className="flex justify-center">
+            <motion.div
+              initial={{ y: "-100%" }}
+              // end={{ x: "100%" }}
+              animate={{ y: 4 }}
+              transition={{ duration: 2 }}
+              className=" text-black rounded"
+            >
+              <span className="flex justify-center gap-2 text-2xl font-semibold pt-4">
+                👋 Hi, I’m <strong>Onuche Atabo</strong>
+              </span>
+              <p className="flex justify-center text-lg">
+                <br></br>{" "}
+                <span className="animate-pulse mr-2">
+                  <strong>IT Support Specialist</strong>{" "}
+                </span>{" "}
+                and{" "}
+                <span className="animate-pulse mx-2">
+                  <strong>Frontend Developer</strong>
+                </span>
+                passionate about solving real-world problems with technology.
+              </p>
+            </motion.div>
           </p>
-          <p>
+          <p className="justify-center text-center pt-4">
             I specialize in:
             <ul>
               <li>
