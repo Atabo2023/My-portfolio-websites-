96c892a5895778d4fef37f9fd24875490aea8898
Atabo2023 <onuchevictor.dev.2023@gmail.com>
2025-11-10 02:59:19 +0100

    modified mobile responsiveness and added data component

--- My-portfolio-websites--main/src/Contactpage.jsx
+++ My-portfolio-websites--main/src/Contactpage.jsx
@@ -3,6 +3,7 @@ import emailjs from "emailjs-com";
 import { FaWhatsapp, FaLinkedin, FaGithub } from "react-icons/fa";
 import { Download } from "lucide-react";
 import { MdEmail } from "react-icons/md";
+import Data from "./Data";
 
 const Contactpage = () => {
   const [loading, setLoading] = useState(false);
@@ -43,7 +44,7 @@ const Contactpage = () => {
     <form
       id="Contactpage"
       onSubmit={sendEmail}
-      className="md:flex gap-12 md:m-10 w-full bg-black max-w-6xl mx-auto drop-shadow-xl rounded-2xl py-10 px-6"
+      className="md:flex gap-12 md:m-10 lg:m-10 bg-black md:mx-auto max-w-7xl mx-auto drop-shadow-xl rounded-2xl py-10 px-6"
     >
       <div className="flex-1 p-4 rounded-xl">
         <h2 className="font-montserrat font-bold text-5xl text-[#ffbd39]">
@@ -106,12 +107,14 @@ const Contactpage = () => {
           // download="Onuche_resume.pdf"
           target="_blank"
           rel="_noopener noreferrer"
-          className="flex items-center md:mx-0 mx-auto mb-8 md:mb-0 gap-2 mt-10 w-48 bg-blue-600 text-white px-2 py-2 rounded-lg shadow-md hover:bg-blue-700 transition-colors duration-300"
+          className="flex items-center mx-auto md:mx-0 mb-8 md:mb-0 gap-2 mt-10 sm:w-48 bg-blue-600 text-white px-2 py-2 rounded-lg shadow-md hover:bg-blue-700 transition-colors duration-300"
         >
           <Download className="w-5 h-5" />
           Download Resume
         </a>
+        <Data />
       </div>
+      <div></div>
       <div className="flex-1 bg-white p-8 gap-8 rounded-xl">
         {" "}
         <h2 className="text-2xl font-bold text-center font-rubik text-black mb-6">
--- My-portfolio-websites--main/tailwind.config.js
+++ My-portfolio-websites--main/tailwind.config.js
@@ -2,6 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    darkMode: "class",
     extend: {
       fontFamily: {
         rubik: ["Rubik", "sans-serif"],
@@ -11,6 +12,27 @@ export default {
 
     fontFamily: {
       exo: ['"Exo 2"', "sans-serif"],
+    },
+
+    keyframes: {
+      twinkle: {
+        "0%, 100%": { opacity: "0.2", transform: "scale(1)" },
+        "50%": { opacity: "1", transform: "scale(1.5)" },
+      },
+      shooting: {
+        "0%": {
+          transform: "translateX(0) translateY(0) scale(1)",
+          opacity: "1",
+        },
+        "100%": {
+          transform: "translateX(200px) translateY(100px) scale(0.5)",
+          opacity: "0",
+        },
+      },
+    },
+    animation: {
+      twinkle: "twinkle 1.5s infinite ease-in-out",
+      shooting: "shooting 2s linear forwards",
     },
   },
 
